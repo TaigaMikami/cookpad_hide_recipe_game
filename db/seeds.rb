23007a193a8c3c759a4db93584b0b9c08d61a10d
TaigaMikami <mikamimikami60@gmail.com>
2018-09-14 13:50:49 +0900

    一旦完成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,17 @@ File.join(File.dirname(__FILE__), 'seeds').tap do |dir|
   require File.join(dir, 'materials.rb')
   require File.join(dir, 'recipes.rb')
 end
+
+Menu.find_by(title: 'アクアパッツァ').tools << Tool.find_by(name: '包丁')
+Menu.find_by(title: 'アクアパッツァ').tools << Tool.find_by(name: 'フライパン')
+Menu.find_by(title: 'アクアパッツァ').tools << Tool.find_by(name: '鍋蓋')
+Menu.find_by(title: 'ラタトゥイユ').tools << Tool.find_by(name: '包丁')
+Menu.find_by(title: 'ラタトゥイユ').tools << Tool.find_by(name: '鍋')
+Menu.find_by(title: '茶碗蒸し').tools << Tool.find_by(name: '鍋')
+Menu.find_by(title: '茶碗蒸し').tools << Tool.find_by(name: '竹串')
+Menu.find_by(title: '茶碗蒸し').tools << Tool.find_by(name: '茶碗')
+Menu.find_by(title: 'ビーフストロガノフ').tools << Tool.find_by(name: '鍋')
+Menu.find_by(title: 'マタンブレ').tools << Tool.find_by(name: 'ラップ')
+Menu.find_by(title: 'マタンブレ').tools << Tool.find_by(name: '耐熱容器')
+Menu.find_by(title: 'ロッソリ').tools << Tool.find_by(name: 'ジップロック')
+Menu.find_by(title: 'ロッソリ').tools << Tool.find_by(name: '電子レンジ')
